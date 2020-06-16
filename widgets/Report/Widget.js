db543c2b42ad2bef4f1ed6fc61f024cee863838d
Freddy Barrera Artiaga <fredy.barrera.a@gmail.com>
2020-06-16 11:16:44 -0400

    Coordenadas mina

--- widgets/Report/Widget.js
+++ widgets/Report/Widget.js
@@ -92,6 +92,37 @@ define([
 				}
 			},
 
+			_onBtnExportClicked: function (){
+				// Select rows from table_id
+				var table_id = 'table-mina';
+				var rows = document.querySelectorAll('table#' + table_id + ' tr');
+				// Construct csv
+				var csv = [];
+				for (var i = 0; i < rows.length; i++) {
+					var row = [], cols = rows[i].querySelectorAll('td, th');
+					for (var j = 0; j < cols.length; j++) {
+						// Clean innertext to remove multiple spaces and jumpline (break csv)
+						var data = cols[j].innerText.replace(/(\r\n|\n|\r)/gm, '').replace(/(\s\s)/gm, ' ')
+						// Escape double-quote with double-double-quote (see https://stackoverflow.com/questions/17808511/properly-escape-a-double-quote-in-csv)
+						data = data.replace(/"/g, '""');
+						// Push escaped string
+						row.push('"' + data + '"');
+					}
+					csv.push(row.join(';'));
+				}
+				var csv_string = csv.join('\n');
+				// Download it
+				var filename = 'export_' + table_id + '_' + new Date().toLocaleDateString() + '.csv';
+				var link = document.createElement('a');
+				link.style.display = 'none';
+				link.setAttribute('target', '_blank');
+				link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv_string));
+				link.setAttribute('download', filename);
+				document.body.appendChild(link);
+				link.click();
+				document.body.removeChild(link);
+			},
+
 			_onBtnConvertClicked: function (){
 
 				if (!projection.isSupported()) {
@@ -130,14 +161,14 @@ define([
 					// Transformo a WGS84
 					var projGeo = projection.project(point, outSpatialReferenceGeo);
 					console.log('projGeo: ', projGeo);
-					var x_wgs84 = projGeo.x.toFixed(6)
-					var y_wgs84 = projGeo.y.toFixed(6)
+					var x_wgs84 = projGeo.x.toFixed(3)
+					var y_wgs84 = projGeo.y.toFixed(3)
 					
 					// Transformo a PSAD_56
 					var projPsad = projection.project(point, outSpatialReferencePsad56);
 					console.log('projPsad: ', projPsad);
-					var x_psad56 = projPsad.x.toFixed(6);
-					var y_psad56 = projPsad.y.toFixed(6);
+					var x_psad56 = projPsad.x.toFixed(3);
+					var y_psad56 = projPsad.y.toFixed(3);
 					
 					// Transformo a UTM
 					var utm = coordinateFormatter.toUtm(projGeo, 'north-south-indicators', true);
@@ -146,17 +177,54 @@ define([
 					var coords_utm = arr[0] + ' ' + arr[1] + ' ' + arr[2];
 					
 					var sms = new SimpleMarkerSymbol().setStyle(SimpleMarkerSymbol.STYLE_CIRCLE).setColor(new Color([255, 0, 0, 0.5]));
-					var attr = { "lon_mina": lon_mina, "lat_mina": lat_mina, "x_wgs84": x_wgs84, "y_wgs84": y_wgs84, "x_psad56": x_psad56, "y_psad56": y_psad56, "coords_utm": coords_utm};
-					var infoTemplate = new InfoTemplate("Coordenadas", "<b>X:</b> ${lon_mina} <br/><b>Y:</b> ${lat_mina} <br/><b>X_WGS84:</b> ${x_wgs84} <br/><b>Y_WGS84:</b> ${y_wgs84}<br/> <b>X_PSAD56:</b> ${x_psad56} <br/><b>Y_PSAD56:</b> ${y_psad56} <br/><b>UTM:</b> ${coords_utm}");
+
+					var attr = { 
+						"lon_mina": lon_mina, 
+						"lat_mina": lat_mina, 
+						"x_wgs84": x_wgs84, 
+						"y_wgs84": y_wgs84, 
+						"x_psad56": x_psad56, 
+						"y_psad56": y_psad56, 
+						"coords_utm": coords_utm
+					};
+
+					var html_infotemplate = '<table cellspacing="0" cellpadding="0" style="border: none;"><tbody>'
+					html_infotemplate += '<tr><td><b>Este Mina (X): </b></td>';
+					html_infotemplate += '<td>${lon_mina}</td></tr>';
+
+					html_infotemplate += '<tr><td><b>Norte Mina (Y): </b></td>';
+					html_infotemplate += '<td>${lat_mina}</td></tr>';
+
+					html_infotemplate += '<tr><td><b>UTM WGS 84 (Huso, X, Y): </b></td>';
+					html_infotemplate += '<td>${coords_utm}</td></tr>';
+
+					html_infotemplate += '<tr><td><b>Este PSAD 56 (X): </b></td>';
+					html_infotemplate += '<td>${x_psad56}</td></tr>';
+
+					html_infotemplate += '<tr><td><b>Norte PSAD 56 (Y): </b></td>';
+					html_infotemplate += '<td>${y_psad56}</td></tr>';
+
+					html_infotemplate += '<tr><td><b>Este WGS 84 (Latitud): </b></td>';
+					html_infotemplate += '<td>${y_wgs84}</td></tr>';
+
+					html_infotemplate += '<tr><td><b>Norte WGS 84 (Longitud): </b></td>';
+					html_infotemplate += '<td>${x_wgs84}</td></tr>';
+
+					html_infotemplate += '</tbody></table>';
+
+					var infoTemplate = new InfoTemplate("Coordenadas", html_infotemplate);
+
 					var graphic = new Graphic(projGeo, sms, attr, infoTemplate);
 					gLayer.add(graphic);
 
 					var tr = document.createElement('tr');
-					tr.innerHTML += '<td>' + x_wgs84 + '</td>'
-					tr.innerHTML += '<td>' + y_wgs84 + '</td>'
+					tr.innerHTML += '<td>' + lon_mina + '</td>'
+					tr.innerHTML += '<td>' + lat_mina + '</td>'
+					tr.innerHTML += '<td>' + coords_utm + '</td>'
 					tr.innerHTML += '<td>' + x_psad56 + '</td>'
 					tr.innerHTML += '<td>' + y_psad56 + '</td>'
-					tr.innerHTML += '<td>' + coords_utm + '</td>'
+					tr.innerHTML += '<td>' + y_wgs84 + '</td>'
+					tr.innerHTML += '<td>' + x_wgs84 + '</td>'
 					var tbody = document.getElementById("tbody-container-coordinates");
 					tbody.appendChild(tr);
 				});
